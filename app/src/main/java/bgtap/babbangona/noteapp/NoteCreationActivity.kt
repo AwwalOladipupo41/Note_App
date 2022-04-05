cf9fc077b6c17417e8e5cf789bbe4d503b4d3a99
Awwal Oladipupo <awwal.oladipupo@babbangona.com>
2022-04-05 04:17:31 +0200

    Note activity file feature

---
+++ app/src/main/java/bgtap/babbangona/noteapp/NoteCreationActivity.kt
@@ -0,0 +1,11 @@
+package bgtap.babbangona.noteapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteCreationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_creation)
+    }
+}
