950a7f91f18888a950e74a4f1360f6a56b695746
Awwal Oladipupo <awwal.oladipupo@babbangona.com>
2022-04-16 00:15:25 +0200

    Updated Main activity , home fragment files

--- app/src/main/java/bgtap/babbangona/noteapp/HomeFragment.kt
+++ app/src/main/java/bgtap/babbangona/noteapp/HomeFragment.kt
@@ -1,13 +1,27 @@
 package bgtap.babbangona.noteapp
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.SearchView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import bgtap.babbangona.noteapp.adapter.NotesAdapter
+import bgtap.babbangona.noteapp.database.NotesDatabase
+import bgtap.babbangona.noteapp.entities.Notes
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.collections.ArrayList
 
+class HomeFragment : BaseFragment() {
 
-class HomeFragment : Fragment() {
+    var arrNotes = ArrayList<Notes>()
+    var notesAdapter: NotesAdapter = NotesAdapter()
+    override val colorView: Any
+        get() = TODO("Not yet implemented")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -19,16 +33,94 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_home, container, false)
+
     }
 
     companion object {
         @JvmStatic
         fun newInstance() =
             HomeFragment().apply {
-                var arguments = Bundle().apply {
-
+                arguments = Bundle().apply {
                 }
             }
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recycler_view.setHasFixedSize(false)
+
+        recycler_view.layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+
+
+        launch {
+            context?.let {
+                var notes = NotesDatabase.getDatabase(it).noteDao().getAllNotes()
+                notesAdapter!!.setData(notes)
+                arrNotes = notes as ArrayList<Notes>
+                recycler_view.adapter = notesAdapter
+            }
+        }
+
+        notesAdapter!!.setOnClickListener(onClicked)
+
+        fabBtnCreateNote.setOnClickListener {
+            replaceFragment(CreateNoteFragment.newInstance(),false)
+        }
+
+        search_view.setOnQueryTextListener( object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+
+                var tempArr = ArrayList<Notes>()
+
+                for (arr in arrNotes){
+                    if (arr.title!!.toLowerCase(Locale.getDefault()).contains(p0.toString())){
+                        tempArr.add(arr)
+                    }
+                }
+
+                notesAdapter.setData(tempArr)
+                notesAdapter.notifyDataSetChanged()
+                return true
+            }
+
+        })
+
+
+    }
+
+
+    private val onClicked = object :NotesAdapter.OnItemClickListener{
+        override fun onClicked(notesId: Int) {
+
+
+            var fragment :Fragment
+            var bundle = Bundle()
+            bundle.putInt("noteId",notesId)
+            fragment = CreateNoteFragment.newInstance()
+            fragment.arguments = bundle
+
+            replaceFragment(fragment,false)
+        }
+
+    }
+
+
+    fun replaceFragment(fragment:Fragment, istransition:Boolean){
+        val fragmentTransition = activity!!.supportFragmentManager.beginTransaction()
+
+        if (istransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.replace(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+    }
+
+
 }
